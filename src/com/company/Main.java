1a73c51ad7d234e234e144fb86bf05f0ac95cb24
Redyx <dr.redyx@gmail.com>
2021-10-11 21:36:02 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    //task 1
+        byte b = 1;
+        short sh = 1;
+        int n = 1;
+        long l = 1l;
+        float f = 1.0f;
+        double d = 1.0;
+        char ch = 1;
+        boolean bool = true;
+        
+    }
+}
