8ab309c0efcf58f752ee009a15930a8997775436
Redyx <dr.redyx@gmail.com>
2021-10-11 21:42:16 +0300

    2 task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    //task 1
+        //task 1
         byte b = 1;
         short sh = 1;
         int n = 1;
-        long l = 1l;
+        long l = 112344132;
         float f = 1.0f;
         double d = 1.0;
         char ch = 1;
         boolean bool = true;
-        
+        //task 2
+        double weightFirstBoxer = 78.2;
+        double weightSecondBoxer = 82.7;
+        System.out.println((weightFirstBoxer+weightSecondBoxer)/2);
+        System.out.println(weightSecondBoxer-weightFirstBoxer);
+
     }
 }
